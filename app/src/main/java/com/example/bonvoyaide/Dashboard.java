39c792ae9508b9d7f303873b0755201393927ff9
darshanpatil324 <darshan.sudhirpatil@accoliteindia.com>
2020-07-04 16:28:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bonvoyaide/Dashboard.java
@@ -0,0 +1,74 @@
+package com.example.bonvoyaide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Dashboard extends AppCompatActivity {
+
+    private ImageView covid;
+    private ImageView trip;
+    private ImageView hotel;
+    private ImageView help;
+    private ImageView stats;
+    private ImageView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        covid = findViewById(R.id.covid);
+        trip = findViewById(R.id.trip);
+        hotel = findViewById(R.id.hotel);
+        help = findViewById(R.id.help);
+        stats = findViewById(R.id.stats);
+        map = findViewById(R.id.map);
+
+        covid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, Covid.class));
+
+            }
+        });
+
+        trip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+        stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+    }
+}
